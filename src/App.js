9a92f40d000c5ce7f7308d595cdb939fa0025e4d
MikiWojak <mikizarowa8@gmail.com>
2023-08-13 12:57:06 +0200

    Add title and info

--- src/App.js
+++ src/App.js
@@ -55,6 +55,11 @@ const App = () => {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+      </p>
+
       <div className="grid">
           {diceElements}
       </div>
